daba7f0a1ffea34eea03aafbe56d5fb5d8a06607
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-02-28 08:48:16 +0100

    Animation between scrolling through the various page sections

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 new WOW().init();
+
+//scrolling to a div
+$(".scroll-to").on("click", function(e) {
+    e.preventDefault();
+    $("body, html").animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 1500);
+
+});
